6c11c51f60b38581a5c11eec15df65740e91ce83
vuralkurt <vuralkurt3535@gmail.com>
2022-02-15 22:37:08 +0100

    ilk

---
+++ src/main/java/com/lecture/carrental/domain/Role.java
@@ -0,0 +1,33 @@
+package com.lecture.carrental.domain;
+
+import com.lecture.carrental.domain.enumeration.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name="roles")
+@Entity
+public class Role {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 30)
+    private UserRole name;
+
+
+    @Override
+    public String toString() {
+        return "" + name + '}';
+    }
+}
---
+++ src/main/java/com/lecture/carrental/domain/enumeration/UserRole.java
@@ -0,0 +1,7 @@
+package com.lecture.carrental.domain.enumeration;
+
+public enum UserRole {
+
+    ROLE_CUSTOMER, ROLE_ADMIN, ROLE_CUSTOMER_SERVICE, ROLE_MANAGER
+
+}
